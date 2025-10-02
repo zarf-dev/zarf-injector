9a0118c0ce253d744abbe33966e3417e093301b9
Austin Abro <austinabro321@gmail.com>
2025-10-02 12:15:06 -0400

    test names
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -753,14 +753,14 @@ mod test {
 
     #[tokio::test]
     #[serial]
-    async fn test_integration() {
+    async fn test_pull_mt() {
         let media_types = [OCI_MIME_TYPE, DOCKER_MEDIA_TYPE];
         for media_type in media_types {
-            test_registry(TEST_IMAGE, media_type).await;
+            test_pull(TEST_IMAGE, media_type).await;
         }
     }
 
-    async fn test_registry(image: &str, media_type: &str) {
+    async fn test_pull(image: &str, media_type: &str) {
         let registry = TestRegistry::new(image).await;
 
         // Assert the files and directory we expect to exist do exist
@@ -857,7 +857,7 @@ mod test {
 
     #[tokio::test]
     #[serial]
-    async fn test_push_integration() {
+    async fn test_push() {
         test_push_with_tag().await;
         test_push_with_sha().await;
     }
